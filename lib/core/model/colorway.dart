d882f2ee1ff40f32fca487482ab9916b3a387fe5
Swapnayg <71422116+iamcrypto@users.noreply.github.com>
2025-02-21 09:13:52 +0530

    update

--- lib/core/model/colorway.dart
+++ lib/core/model/colorway.dart
@@ -3,10 +3,20 @@ import 'package:flutter/cupertino.dart';
 class Colorway {
   String name;
   Color color;
+  String color_code;
+  bool selected;
 
-  Colorway({required this.name, required this.color});
+  Colorway(
+      {required this.name,
+      required this.color,
+      required this.color_code,
+      required this.selected});
 
   factory Colorway.fromJson(Map<String, dynamic> json) {
-    return Colorway(name: json['name'], color: json['color']);
+    return Colorway(
+        name: json['name'],
+        color: json['color'],
+        color_code: json['color_code'],
+        selected: json['selected']);
   }
 }
